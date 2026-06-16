09d29e23bed6caa1560a784654c38656c8721f81
YooDxE3 <andreox27@gmail.com>
2026-06-16 17:36:09 -0300

    adicionando spring

---
+++ backend/spring/src/test/java/hackthon/grupo3/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package hackthon.grupo3.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
