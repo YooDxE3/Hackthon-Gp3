ff4e33a1c6378935fcd0a8dcd773845a0d26d08c
GHFriedrichsen <gabriel.h.friedrichsen@gmail.com>
2026-06-18 20:23:50 -0300

    feat(Authetication) autenticacao completa -- testes pendentes

--- backend/spring/src/main/java/hackthon/grupo3/spring/config/SecurityConfig.java
+++ backend/spring/src/main/java/hackthon/grupo3/spring/config/SecurityConfig.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -40,6 +42,11 @@ public class SecurityConfig {
     public JwtEncoder jwtEncoder() {
         SecretKey chave = new SecretKeySpec(jwtSecret.getBytes(), "HmacSHA256");
         return new NimbusJwtEncoder(new ImmutableSecret<>(chave));
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
     @Bean
