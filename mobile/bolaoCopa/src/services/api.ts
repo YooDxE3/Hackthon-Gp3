59274f0715d8fc9fb5aaa1c104dd3f5f1ed127af
YooDxE3 <andreox27@gmail.com>
2026-06-22 20:26:46 -0300

    Subindo versao para rodar expo go pc

--- mobile/bolaoCopa/src/services/api.ts
+++ mobile/bolaoCopa/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { router } from 'expo-router';
 
-const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.3.5:8080/api';
+const API_BASE_URL = process.env.EXPO_PUBLIC_API_URL || 'http://192.168.3.14:8080/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
